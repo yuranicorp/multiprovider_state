f2faeb41919c8e7501ee511e479cd309636948eb
yusriltakeuchi <yusriltakeuchi@gmail.com>
2020-10-01 12:34:44 +0700

    Refactoring code

--- lib/core/viewmodels/money/money_provider.dart
+++ lib/core/viewmodels/money/money_provider.dart
@@ -4,6 +4,7 @@ class MoneyProvider with ChangeNotifier {
   int _balance = 5000;
   int get balance => _balance;
 
+  /// Decrease balance after add item to cart
   void decreaseBalance(int value) {
     _balance -= value;
     notifyListeners();
